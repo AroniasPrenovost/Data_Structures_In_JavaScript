d657f0d3cd3343410c278c5920f0370318d38eac
AroniasPrenovost <Aroniascoder@gmail.com>
2017-07-22 23:06:29 -0700

    finished linked list exercise

--- linked_list.js
+++ linked_list.js
@@ -22,11 +22,61 @@ function LinkedList() {
  function Node(value, next, prev) {
  	this.value = value;
  	this.next = next;
- 	this.previous = prev;
+ 	this.prev = prev;
  }
 
-// create demo linked list 
-var node1 = new Node(100, 'node2', null);
-console.log(node1);
-// { value: 100, next: 'node2', previous: null }
-// => undefined
+LinkedList.prototype.addToHead = function(value) {  // creates method
+var newNode = new Node(value, this.head, null); // creates new node 
+	if (this.head) this.head.prev = newNode; // handles case if node
+	else this.tail = newNode; // if empty, set the end to this node 
+	this.head = newNode; // whether empty or not, we want our new node at front of list
+};
+
+// similar to add to head, just reverse
+LinkedList.prototype.addToTail = function(value) {
+	var newNode = new Node(value, null, this.tail);
+	if (this.tail) this.tail.next = newNode;
+	else this.head = newNode;
+	this.tail = newNode;
+};
+
+LinkedList.prototype.removeHead = function() {
+	if (!this.head) return null;
+	var val = this.head.value;
+	this.head = this.head.next;
+	if (this.head) this.head.prev = null;
+	else this.tail = null; 
+	return val;
+};
+
+LinkedList.prototype.removeTail = function() {
+	if (!this.tail) return null;
+	var val = this.tail.value;
+	this.tail = this.tail.prev;
+	if (this.tail) this.tail.next = null;
+	else this.head = null;
+	return val;
+}
+
+LinkedList.prototype.search = function(searchValue) {
+  var currentNode = this.head;
+ while(currentNode) {
+   if (currentNode.value === searchValue) return currentNode.value;
+  currentNode = currentNode.next; 
+ }
+ return null;
+};
+
+var myLL = new LinkedList();
+
+myLL.addToHead(123);
+myLL.addToHead(70);
+myLL.addToTail('world');
+myLL.addToTail(19);
+
+console.log(myLL.search(10));
+
+/*
+var ll = new LinkedList();
+ll.search();
+*/
